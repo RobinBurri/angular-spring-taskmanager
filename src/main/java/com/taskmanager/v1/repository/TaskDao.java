32194292b60fc541e95ccacee1cb9ed68bcc57bc
Robin Burri <robinburri@tutanota.com>
2023-12-20 20:03:27 +0100

    add TaskDao, UserDao @Repository

--- src/main/java/com/taskmanager/v1/repository/TaskDao.java
+++ src/main/java/com/taskmanager/v1/repository/TaskDao.java
@@ -1,9 +1,11 @@
 package com.taskmanager.v1.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.taskmanager.v1.entity.Task;
 
+@Repository
 public interface TaskDao extends CrudRepository<Task, Integer>{
     
 }
--- src/main/java/com/taskmanager/v1/repository/UserDao.java
+++ src/main/java/com/taskmanager/v1/repository/UserDao.java
@@ -1,9 +1,11 @@
 package com.taskmanager.v1.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.taskmanager.v1.entity.User;
 
+@Repository
 public interface UserDao extends CrudRepository<User, Integer>{
     
 }
